c93e08fbe49adf605b7d006be6a10b49758dbf38
keillani <keillani@github.com>
2020-11-16 18:05:19 -0300

    aula 03 variaveis globais e objects

--- src/br/com/alura/bytebank/modelo/Conta.kt
+++ src/br/com/alura/bytebank/modelo/Conta.kt
@@ -1,11 +1,33 @@
 package br.com.alura.bytebank.modelo
 
+//import totalContas
+
+//var totalContas = 0
+//private set
+
 abstract class Conta(
     var titular: Cliente,
     val numero: Int
 ) {
     var saldo = 0.0
         protected set
+
+    //companion object declaration == Singleton (objetivo de criar uma instância única)
+    companion object{
+
+        var total=0
+        private set
+
+        fun incrementa(){
+            total++
+        }
+    }
+
+    init {
+        println("Criando conta")
+//        totalContas++
+        total++ //conpanion object privado portanto so podera ser usado dentro da classe
+    }
 
     fun deposita(valor: Double) {
         if (valor > 0) {
@@ -22,5 +44,44 @@ abstract class Conta(
             return true
         }
         return false
+    }
+}
+
+class ContaCorrente(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+
+//    init {
+////        totalContas++
+//    }
+
+    override fun saca(valor: Double) {
+        val valorComTaxa = valor + 0.1
+        if(this.saldo >= valorComTaxa){
+            this.saldo -= valorComTaxa
+        }
+    }
+}
+
+class ContaPoupanca(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+
+//    init {
+////        totalContas++
+//    }
+
+    override fun saca(valor: Double) {
+        if(this.saldo >= valor){
+            this.saldo -= valor
+        }
     }
 }
