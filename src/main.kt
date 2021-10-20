1b026015b123823e8b3719e82002203b3131140b
keillani <keillani@github.com>
2021-10-20 19:31:59 -0300

    aula 05 reutilizando e sobreescrevendo metodos

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,17 @@
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    val endereco=Endereco()
-
-    val objeto=Any()
-    imprime(objeto)
-
-    imprime(1)
-    imprime(1.0)
-    imprime(endereco)
+    val endereco=Endereco(logradouro = "teste", numero = 1, cep = "00000-000")
+    val enderecoNovo=Endereco(logradouro = "teste", numero = 1, cep = "00000-000")
+    
+    val testeEquals = endereco.equals(enderecoNovo)
+    val testeHashCode = endereco.hashCode()
+    val testeHashCode2 = enderecoNovo.hashCode()
+    println(endereco)
+    println(enderecoNovo)
+    println(testeEquals)
+    println(testeHashCode)
+    println(testeHashCode2)
 }
 
 fun imprime(valor: Any): Any {
